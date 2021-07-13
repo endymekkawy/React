85eadbb07831e70ba2da9f700f68dd3ad61bbb3c
endymekkawy <74079056+endymekkawy@users.noreply.github.com>
2021-07-13 21:43:38 +0200

    Add files via upload

---
+++ 05_04/finished/hello-react/build/precache-manifest.b2bdf794b3b95cbc7cfbae4081049c5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dce57293d35c491141ce20f163159fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6374d3c5fa1c4130d89b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5272aea1468cb4ad8e5c",
+    "url": "/static/js/2.baa36876.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.baa36876.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6374d3c5fa1c4130d89b",
+    "url": "/static/js/main.effe533b.chunk.js"
+  },
+  {
+    "revision": "c31ecb14e299df56c6e9",
+    "url": "/static/js/runtime-main.d058ec17.js"
+  }
+]);
